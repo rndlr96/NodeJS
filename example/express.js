3aaae3f7a1e6b61c7e21a17e2849ba898e327b4d
JungGuIk <rndlr96@gmail.com>
2018-07-10 15:05:54 +0900

    "[ADD]express example upload"

---
+++ example/express.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+
+let users = [
+  {
+    id: 1,
+    name: 'alice'
+  },
+  {
+    id: 2,
+    name: 'bek'
+  },
+  {
+    id: 3,
+    name: 'chris'
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+  return res.json(users);
+})
+
+app.get('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (!id) {
+    return res.status(400).json({error: 'Incorrect id'});
+  }
+
+  let user = users.filter(user => user.id === id)[0]
+  if(!user){
+    return res.status(404).json({error: 'Unknown user'});
+  }
+
+  return res.json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if(!id) {
+    return res.status(400).json({error: 'Incorrect id'});
+  }
+
+  const userIdx = users.findIndex(user => {
+    return user.id === id;
+  });
+  if (userIdx === -1) {
+    return res.status(404).json({error: 'Unknown user'});
+  }
+
+  users.splice(userIdx, 1);
+  res.status(204).send();
+});
+
+app.post('/users', (req, res) => {
+
+})
+
+app.listen(3000, () => {
+  console.log('Example');
+});
