91402d29225e6006945daccc6649c4cb586531f9
JungGuIk <rndlr96@gmail.com>
2018-07-16 14:37:12 +0900

    [MOD] put API function updata

--- REST_API/API/Restful/router/main.js
+++ REST_API/API/Restful/router/main.js
@@ -26,7 +26,6 @@ module.exports = function(app, fs)
          var result = {  };
          var username = req.params.username;
 
-         // CHECK REQ VALIDITY
          if(!req.body["password"] || !req.body["name"]){
              result["success"] = 0;
              result["error"] = "invalid request";
@@ -34,26 +33,47 @@ module.exports = function(app, fs)
              return;
          }
 
-         // LOAD DATA & CHECK DUPLICATION
          fs.readFile( __dirname + "/../data/data.json", 'utf8',  function(err, data){
              var users = JSON.parse(data);
              if(users[username]){
-                 // DUPLICATION FOUND
+
                  result["success"] = 0;
                  result["error"] = "duplicate";
                  res.json(result);
                  return;
              }
 
-             // ADD TO DATA
              users[username] = req.body;
 
-             // SAVE DATA
              fs.writeFile(__dirname + "/../data/data.json",
                           JSON.stringify(users, null, '\t'), "utf8", function(err, data){
                  result = {"success": 1};
                  res.json(result);
              })
          })
+     });
+
+     app.put('/modUser/:username', function(req, res){
+         var result = { };
+         var username = req.params.username;
+
+         if(!req.body["password"] || !req.body["name"]){
+             result["success"] = 0;
+             result["error"] = "invalid request";
+             res.json(result);
+             return;
+         }
+
+         fs.readFile( __dirname + "/../data/" + "data.json", 'utf8', function(err, data){
+             var users = JSON.parse(data);
+
+             if(users[username]){
+                 users[username] = req.body;
+                 fs.writeFile(__dirname + "/../data/" + "data.json", JSON.stringify(users, null, '\t'), 'utf8', function(err, data){
+                     result = {"sucess": 1};
+                     res.json(result);
+                 })
+             }
+         });
      });
 }
